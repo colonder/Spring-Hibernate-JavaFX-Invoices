06ea625ff6cd130eb4cecab751e8338131b71a81
kuba <kuba.kustra@gmail.com>
2017-07-08 00:53:28 +0200

    Refined customers management window

--- src/main/java/com/UI/controllers/ManageCustomersDialogPresenter.java
+++ src/main/java/com/UI/controllers/ManageCustomersDialogPresenter.java
@@ -1,13 +1,11 @@
 package com.UI.controllers;
 
-import com.entity.Customer;
+import com.entity.Customer.CustomerProps;
 import com.entity.PaymentMethod;
 import com.utilities.classes.CustomersList;
 import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.util.Duration;
 import org.springframework.stereotype.Component;
@@ -15,17 +13,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class ManageCustomersDialogPresenter
 {
-    @FXML TableView<Customer.CustomerProps> customersListTableView;
-    @FXML TableColumn<Customer.CustomerProps, String> lastNameCol;
-    @FXML TableColumn<Customer.CustomerProps, String> firstNameCol;
-    @FXML TableColumn<Customer.CustomerProps, String> companyNameCol;
-    @FXML TableColumn<Customer.CustomerProps, String> IdNumberCol;
-    @FXML TableColumn<Customer.CustomerProps, String> addressCol;
-    @FXML TableColumn<Customer.CustomerProps, String> postalCodeCol;
-    @FXML TableColumn<Customer.CustomerProps, String> cityCol;
-    @FXML TableColumn<Customer.CustomerProps, Boolean> considerCountingCol;
-    @FXML TableColumn<Customer.CustomerProps, PaymentMethod> paymentMethodCol;
-    @FXML TableColumn<Customer.CustomerProps, String> aliasCol;
+    //region @FXML objects
+    @FXML TableView<CustomerProps> customersListTableView;
+    @FXML TableColumn<CustomerProps, String> lastNameCol;
+    @FXML TableColumn<CustomerProps, String> firstNameCol;
+    @FXML TableColumn<CustomerProps, String> companyNameCol;
+    @FXML TableColumn<CustomerProps, String> IdNumberCol;
+    @FXML TableColumn<CustomerProps, String> addressCol;
+    @FXML TableColumn<CustomerProps, String> postalCodeCol;
+    @FXML TableColumn<CustomerProps, String> cityCol;
+    @FXML TableColumn<CustomerProps, Boolean> considerCountingCol;
+    @FXML TableColumn<CustomerProps, PaymentMethod> paymentMethodCol;
+    @FXML TableColumn<CustomerProps, String> aliasCol;
+    @FXML ComboBox filterComboBox;
+    @FXML TextField filterTextField;
+    @FXML Button filterBtn;
+    @FXML Button newCustomerBtn;
+    @FXML Button editCustomerBtn;
+    @FXML Button removeCustomerBtn;
+    //endregion
 
     @FXML
     public void initialize()
@@ -42,58 +48,15 @@ public class ManageCustomersDialogPresenter
         aliasCol.setCellValueFactory(new PropertyValueFactory<>("aliasProp"));
 
         //TODO: add list sorting capability
+        //TODO: write code for new controls
 
         customersListTableView.setItems(CustomersList.customerList);
+        customersListTableView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 
         PauseTransition pause = new PauseTransition(Duration.seconds(2));
         TextField searchTextBox = new TextField();
         searchTextBox.setPromptText("Wyszukaj kontrahenta");
         searchTextBox.textProperty().addListener((observable, oldValue, newValue) -> {
-
-            // to decrease numbers of database queries
-            pause.playFromStart();
-            /*switch (comboBox.getSelectionModel().getSelectedItem())
-            {
-                case "Imię":
-                    data.addAll(customerService.findAllByFirstNameContaining(newValue));
-                    break;
-
-                case "Nazwisko":
-                    data.addAll(customerService.findAllByLastNameContaining(newValue));
-                    break;
-
-                case "Alias":
-                    data.addAll(customerService.findAllByAliasContaining(newValue));
-                    break;
-
-                case "Nazwa firmy":
-                    data.addAll(customerService.findAllByCompanyNameContaining(newValue));
-                    break;
-
-                case "PESEL/NIP":
-                    data.addAll(customerService.findAllByTaxIdentifierContaining(newValue));
-                    break;
-
-                case "Adres":
-                    data.addAll(customerService.findAllByAddressContaining(newValue));
-                    break;
-
-                case "Kod pocztowy":
-                    data.addAll(customerService.findAllByPostalCodeContaining(newValue));
-                    break;
-
-                case "Miejscowość":
-                    data.addAll(customerService.findAllByCityContaining(newValue));
-                    break;
-
-                case "Sposób zapłaty":
-                    data.addAll(customerService.findAllByPaymentMethod(newValue));
-                    break;
-
-                case "Uwzględnij nr faktury":
-                    data.addAll(customerService.findAllByIncludeInCount(newValue));
-                    break;
-            }*/
 
         });
     }
