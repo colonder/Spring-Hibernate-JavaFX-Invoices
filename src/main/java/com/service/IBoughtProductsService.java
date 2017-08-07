379148fb4ca532048cbf3598aab6808e4ba57e7a
kuba <kuba.kustra@gmail.com>
2017-08-07 23:06:52 +0200

    Completed service layers for customers and bought products

---
+++ src/main/java/com/service/IBoughtProductsService.java
@@ -0,0 +1,15 @@
+package com.service;
+
+import com.entity.BoughtProducts;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+public interface IBoughtProductsService
+{
+    List<BoughtProducts> findAll();
+    BoughtProducts save(BoughtProducts product);
+    void delete(BoughtProducts customer);
+    int update(BigDecimal quantity, BigDecimal netValue, BigDecimal taxValue, BigDecimal grossValue, Date sellDate, int id);
+}
