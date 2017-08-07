b3053a4afc7543ce2dde4af7e8b5af866dca3c94
kuba <kuba.kustra@gmail.com>
2017-08-07 23:48:37 +0200

    Completed service implementation for invoices

--- src/main/java/com/serviceImples/InvoiceServImp.java
+++ src/main/java/com/serviceImples/InvoiceServImp.java
@@ -1,4 +1,49 @@
 package com.serviceImples;
 
-public class InvoiceServImp {
+import com.entity.Invoice;
+import com.entity.enums.InvoiceStatus;
+import com.entity.enums.PaymentMethod;
+import com.repositories.IInvoiceRepository;
+import com.service.IInvoiceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+public class InvoiceServImp implements IInvoiceService {
+    @Autowired
+    private IInvoiceRepository invoiceRepository;
+
+    @Override
+    public List<Invoice> findAll() {
+        return invoiceRepository.findAll();
+    }
+
+    @Override
+    @Transactional
+    public Invoice save(Invoice invoice) {
+        return invoiceRepository.save(invoice);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Invoice invoice) {
+        invoiceRepository.delete(invoice);
+    }
+
+    @Override
+    @Transactional
+    public int update(String seller, BigDecimal paidAmount, PaymentMethod method, Date paymentDate,
+                      Date paymentDeadline, InvoiceStatus status, Date lastModified, String notes, int id) {
+        return invoiceRepository.update(seller, paidAmount, method, paymentDate, paymentDeadline, status, lastModified,
+                notes, id);
+    }
+
+    @Override
+    @Transactional
+    public int updateSent(Date sentDate, int id) {
+        return invoiceRepository.updateSent(sentDate, id);
+    }
 }
