a72868d2d728c4c5faff0aed6042328f2f4501f8
kuba <kuba.kustra@gmail.com>
2017-08-07 20:18:26 +0200

    Mapped Product entity class

--- src/main/java/com/entity/Product.java
+++ src/main/java/com/entity/Product.java
@@ -1,4 +1,35 @@
 package com.entity;
 
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "product_name", nullable = false)
+    private String productName;
+
+    @Column(name = "net_price", nullable = false)
+    private BigDecimal netPrice;
+
+    @Column(name = "tax_rate", nullable = false)
+    private BigDecimal taxRate;
+
+    @Column(name = "online_sell", nullable = false)
+    private boolean onlineSell;
+
+    @Column(name = "is_service", nullable = false)
+    private boolean isService;
+
+    @Column(name = "is_active", nullable = false)
+    private boolean isActive;
+
+    @ManyToOne
+    @JoinColumn(name = "warehouse_item_id", referencedColumnName = "id")
+    private Warehouse warehouse;
 }
