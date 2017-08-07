5a652acf836a8c70fa34453073364dc3ab692244
kuba <kuba.kustra@gmail.com>
2017-08-07 21:58:13 +0200

    Completed Warehouse repo

--- src/main/java/com/repositories/IWarehouseRepository.java
+++ src/main/java/com/repositories/IWarehouseRepository.java
@@ -2,8 +2,18 @@ package com.repositories;
 
 import com.entity.Warehouse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Date;
 
 public interface IWarehouseRepository extends JpaRepository<Warehouse, Integer>
 {
+    @Modifying
+    @Query("UPDATE Warehouse w SET w.sold =?1, w.available =?2  WHERE id =?3")
+    int updateAmount(int sold, int available, int id);
 
+    @Modifying
+    @Query("UPDATE Warehouse w SET w.productCode =?1, w.lastSellDate =?3 WHERE w.id =?4")
+    int update(String productCode, Date lastSellDate, int id);
 }
