49c72f41165b5bea39b97b17fcb99b1cb0123d15
kuba <kuba.kustra@gmail.com>
2017-08-07 23:50:43 +0200

    Completed service implementation for product

--- src/main/java/com/serviceImples/ProductServImp.java
+++ src/main/java/com/serviceImples/ProductServImp.java
@@ -1,4 +1,35 @@
 package com.serviceImples;
 
-public class ProductServImp {
+import com.entity.Product;
+import com.repositories.IProductRepository;
+import com.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class ProductServImp implements IProductService {
+    @Autowired
+    private IProductRepository productRepository;
+
+    @Override
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public void delete(Product product) {
+        productRepository.delete(product);
+    }
+
+    @Override
+    public int update(String productName, BigDecimal netPrice, BigDecimal taxRate, boolean onlineSell,
+                      boolean isService, boolean isActive, int id) {
+        return productRepository.update(productName, netPrice, taxRate, onlineSell, isService, isActive, id);
+    }
 }
