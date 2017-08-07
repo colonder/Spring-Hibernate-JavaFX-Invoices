8e3746c23ca31997f3ece6d73aa8b1c3db8146ae
kuba <kuba.kustra@gmail.com>
2017-08-07 23:44:55 +0200

    Corrected service implementation and layer for customer

--- src/main/java/com/service/ICustomerService.java
+++ src/main/java/com/service/ICustomerService.java
@@ -1,14 +1,21 @@
 package com.service;
 
 import com.entity.Customer;
+import com.entity.enums.ClientType;
 import com.entity.enums.PaymentMethod;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
 public interface ICustomerService {
     List<Customer> findAll();
     Customer save(Customer customer);
     void delete(Customer customer);
-    int update(String lastName, String firstName, String companyName, String taxIdentifier, String address,
-               String postalCode, String city, PaymentMethod paymentMethod, boolean includeInCount, String alias, int id);
+    int update(String alias, String lastName, String firstName, int personalId, String taxIdentifier, String email,
+               String address, String postalCode, String city, int telephone, int cellPhone, int fax, String tag,
+               PaymentMethod defaultPaymentMethod, String country, ClientType type, int companySpecialNumber,
+               String bankName, String defaultCurrency, int bankAccountNumber, BigDecimal defaultDiscount,
+               Date defaultPaymentDateDays, int id);
+    int updateNewPurchase(Date lastPurchaseDate, int id);
 }
--- src/main/java/com/serviceImples/CustomerServImp.java
+++ src/main/java/com/serviceImples/CustomerServImp.java
@@ -1,29 +1,55 @@
 package com.serviceImples;
 
 import com.entity.Customer;
+import com.entity.enums.ClientType;
 import com.entity.enums.PaymentMethod;
+import com.repositories.ICustomerRepository;
 import com.service.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
 public class CustomerServImp implements ICustomerService {
+    @Autowired
+    private ICustomerRepository customerRepository;
+
+
     @Override
     public List<Customer> findAll() {
-        return null;
+        return customerRepository.findAll();
     }
 
     @Override
+    @Transactional
     public Customer save(Customer customer) {
-        return null;
+        return customerRepository.save(customer);
     }
 
     @Override
+    @Transactional
     public void delete(Customer customer) {
-
+        customerRepository.delete(customer);
     }
 
     @Override
-    public int update(String lastName, String firstName, String companyName, String taxIdentifier, String address, String postalCode, String city, PaymentMethod paymentMethod, boolean includeInCount, String alias, int id) {
-        return 0;
+    @Transactional
+    public int update(String alias, String lastName, String firstName, int personalId, String taxIdentifier,
+                      String email, String address, String postalCode, String city, int telephone, int cellPhone,
+                      int fax, String tag, PaymentMethod defaultPaymentMethod, String country, ClientType type,
+                      int companySpecialNumber, String bankName, String defaultCurrency, int bankAccountNumber,
+                      BigDecimal defaultDiscount, Date defaultPaymentDateDays, int id) {
+        return customerRepository.update(alias, lastName, firstName, personalId, taxIdentifier, email, address,
+                postalCode, city, telephone, cellPhone, fax, tag, defaultPaymentMethod, country, type,
+                companySpecialNumber, bankName, defaultCurrency, bankAccountNumber, defaultDiscount,
+                defaultPaymentDateDays, id);
+    }
+
+    @Override
+    @Transactional
+    public int updateNewPurchase(Date lastPurchaseDate, int id) {
+        return customerRepository.updateNewPurchase(lastPurchaseDate, id);
     }
 }
