5ea3ae8c0c336cbb730e05ba392a925f0247f1b2
kuba <kuba.kustra@gmail.com>
2017-08-07 20:15:06 +0200

    Mapped Product entity class

--- src/main/java/com/entity/BoughtProducts.java
+++ src/main/java/com/entity/BoughtProducts.java
@@ -1,4 +1,37 @@
 package com.entity;
 
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Date;
+
+@Entity
+@Table(name = "bought_products")
 public class BoughtProducts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "quantity", nullable = false)
+    private int quantity;
+
+    @Column(name = "net_value", nullable = false)
+    private BigDecimal netValue;
+
+    @Column(name = "tax_value", nullable = false)
+    private BigDecimal taxValue;
+
+    @Column(name = "gross_value", nullable = false)
+    private BigDecimal grossValue;
+
+    @Column(name = "sell_date", nullable = false)
+    private Date sellDate;
+
+    @ManyToOne
+    @JoinColumn(name = "invoice_id", referencedColumnName = "id")
+    private Invoice invoice;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
+    private Product product;
 }
