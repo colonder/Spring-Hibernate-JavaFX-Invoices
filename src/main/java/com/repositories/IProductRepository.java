177ff475c91b7f02d45d0aa5bab1308cf8c488da
kuba <kuba.kustra@gmail.com>
2017-08-07 22:00:31 +0200

    Added findAll methods to products and warehouse repos

--- src/main/java/com/repositories/IProductRepository.java
+++ src/main/java/com/repositories/IProductRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public interface IProductRepository extends JpaRepository<Product, Integer>
 {
+    List<Product> findAll();
+
     @Modifying
     @Query("UPDATE Product p SET p.productName =?1, p.netPrice =?2, p.taxRate =?3, p.onlineSell =?4, p.isService =?5," +
             "p.isActive =?6 WHERE p.id =?7")
--- src/main/java/com/repositories/IWarehouseRepository.java
+++ src/main/java/com/repositories/IWarehouseRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Date;
+import java.util.List;
 
 public interface IWarehouseRepository extends JpaRepository<Warehouse, Integer>
 {
+    List<Warehouse> findAll();
+
     @Modifying
     @Query("UPDATE Warehouse w SET w.sold =?1, w.available =?2  WHERE id =?3")
     int updateAmount(int sold, int available, int id);
