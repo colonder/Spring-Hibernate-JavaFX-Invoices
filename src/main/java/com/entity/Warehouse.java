1ac61fea85c1179ae4605c3937e65ffd49aaaec9
kuba <kuba.kustra@gmail.com>
2017-08-07 20:20:52 +0200

    Mapped Warehouse entity class

--- src/main/java/com/entity/Warehouse.java
+++ src/main/java/com/entity/Warehouse.java
@@ -1,4 +1,29 @@
 package com.entity;
 
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "warehouse")
 public class Warehouse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    // id a product is some liquid, then it needs to be BigDecimal
+    @Column(name = "sold", nullable = false)
+    private int sold;
+
+    @Column(name = "available", nullable = false)
+    private int available;
+
+    @Column(name = "product_code", nullable = true)
+    private String productCode;
+
+    @Column(name = "creation_date", nullable = false)
+    private Date creationDate;
+
+    @Column(name = "last_sell_date", nullable = true)
+    private Date lastSellDate;
 }
