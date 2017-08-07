912795eb6dd3266a86514f9be855dc982aa0b1b5
kuba <kuba.kustra@gmail.com>
2017-08-07 21:11:50 +0200

    Added findAll to invoice repo

--- src/main/java/com/repositories/ICustomerRepository.java
+++ src/main/java/com/repositories/ICustomerRepository.java
@@ -1,19 +1,11 @@
 package com.repositories;
 
 import com.entity.Customer;
-import com.entity.enums.PaymentMethod;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ICustomerRepository extends JpaRepository<Customer, Integer> {
     List<Customer> findAll();
 
-    @Modifying
-    @Query("UPDATE Customer c SET c.lastName=?1, c.firstName =?2, c.companyName =?3, c.taxIdentifier =?4, c.address =?5," +
-            "c.postalCode =?6, c.city =?7, c.paymentMethod =?8, c.includeInCount =?9, c.alias =?10 WHERE c.id =?11")
-    int update(String lastName, String firstName, String companyName, String taxIdentifier, String address,
-               String postalCode, String city, PaymentMethod paymentMethod, boolean includeInCount, String alias, int id);
 }
--- src/main/java/com/repositories/IInvoiceRepository.java
+++ src/main/java/com/repositories/IInvoiceRepository.java
@@ -9,8 +9,10 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 public interface IInvoiceRepository extends JpaRepository<Invoice, Integer> {
+    List<Invoice> findAll();
     @Modifying
     @Query("UPDATE Invoice i SET i.seller =?1, i.paidAmount =?2, i.paymentMethod=?3, i.paymentDate=?4, " +
             "i.paymentDeadline =?5, i.status =?6, i.lastModified =?7, i.notes =?8 WHERE i.id =?9")
