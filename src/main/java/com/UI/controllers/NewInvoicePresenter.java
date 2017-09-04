4e5f1ca944e61186b1202d882cc0412615f5b90d
kuba <kuba.kustra@gmail.com>
2017-09-05 00:00:17 +0200

    Added FXML fields to new invoice controller

--- src/main/java/com/UI/controllers/NewInvoicePresenter.java
+++ src/main/java/com/UI/controllers/NewInvoicePresenter.java
@@ -2,27 +2,86 @@ package com.UI.controllers;
 
 import com.entity.BaseAbstractEntity;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NewInvoicePresenter implements IInitializableFromEntity {
 
+    //region FXML fields
+    @FXML private ComboBox<String> typeComboBox;
+    @FXML private TextField numberTxtFld;
+    @FXML private DatePicker issueDatePicker;
+    @FXML private TextField locationTxtFld;
+    @FXML private DatePicker saleDatePicker;
+    @FXML private TextField sellerTxtFld;
+    @FXML private TextField sellerTaxIdTxtFld;
+    @FXML private TextField sellerAddressTxtFld;
+    @FXML private TextField sellerPostalCodeTxtFld;
+    @FXML private TextField sellerCityTxtFld;
+    @FXML private TextField accountTxtFld;
+    @FXML private TextField bankTxtFld;
+    @FXML private TextField sellerEmailTxtFld;
+    @FXML private TextField sellerPhoneTxtFld;
+    @FXML private TextField sellerFaxTxtFld;
+    @FXML private Button loadFromDatabaseBtn;
+    @FXML private TextField buyerTxtFld;
+    @FXML private TextField buyerTaxIdTxtFld;
+    @FXML private TextField buyerAddressTxtFld;
+    @FXML private TextField buyerPostalCodeTxtFld;
+    @FXML private TextField buyerCityTxtFld;
+    @FXML private TextField buyerEmailTxtFld;
+    @FXML private TextField buyerPhoneTxtFld;
+    @FXML private ComboBox<String> countryComboBox;
     @FXML private Button addItemBtn;
-    @FXML private Button removeItem0Btn;
     @FXML private GridPane itemsGridPane;
+    @FXML private ComboBox<String> paymentMethodComboBox;
+    @FXML private ComboBox<String> paymentDateComboBox;
+    @FXML private ComboBox<String> statusComboBox;
+    @FXML private TextField paidAmountTxtFld;
+    @FXML private TextField issuerNameTxtFld;
+    @FXML private TextField receiverNameTxtFld;
+    @FXML private TextArea remarksTextArea;
+    @FXML private ComboBox<String> currencyComboBox;
+    @FXML private ComboBox<String> languageComboBox;
+    @FXML private TitledPane optionsTitledPane;
+    @FXML private ComboBox<String> calculateValsComboBox;
+    @FXML private ComboBox<String> showUnitPriceComboBox;
+    @FXML private TextField paidDateTxtFld;
+    @FXML private ComboBox<String> calculateTotalComboBox;
+    @FXML private TextField labelTxtFld;
+    //endregion
 
     @FXML
     public void initialize()
     {
-        addItemBtn.setOnAction(event -> {
+        initComboBoxes();
+        initSeller();
+        initBuyer();
+        initProductsPane();
+        initOptions();
+    }
 
-        });
+    private void initOptions() {
+    }
+
+    private void initProductsPane() {
+    }
+
+    private void initBuyer() {
+    }
+
+    private void initSeller() {
+    }
+
+    private void initComboBoxes() {
     }
 
     @Override
     public void initializeFields(BaseAbstractEntity entity) {
 
     }
+
+
 }
