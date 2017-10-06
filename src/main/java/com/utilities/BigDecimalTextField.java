04cdd0e7360337de50a4f59feb558004c315ad46
kuba <kuba.kustra@gmail.com>
2017-10-07 01:24:02 +0200

    Added two text numeric text fields

--- src/main/java/com/utilities/BigDecimalTextField.java
+++ src/main/java/com/utilities/BigDecimalTextField.java
@@ -1,6 +1,7 @@
 package com.utilities;
 
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
 import javafx.util.converter.BigDecimalStringConverter;
@@ -15,17 +16,36 @@ public class BigDecimalTextField extends TextField {
     {
         super();
         this.setTextFormatter(new TextFormatter<>(new BigDecimalStringConverter()));
-        this.value = new SimpleObjectProperty<>();
+        this.value = new SimpleObjectProperty<>(BigDecimal.ZERO);
+        this.focusedProperty().addListener(observable -> {
+            if (!this.getText().isEmpty()) {
+                try {
+                    this.setValue(new BigDecimal(this.getText()));
+                } catch (NumberFormatException e) {
+                    Alert alert = new Alert(Alert.AlertType.WARNING);
+                    alert.setTitle("Conversion error");
+                    alert.setHeaderText("An error occurred while entering the value");
+                    alert.setContentText("It seems that the you have entered wrong type of the value");
+                    alert.showAndWait();
+                }
+            }
+            else
+                this.setValue(BigDecimal.ZERO);
+        });
     }
 
     public BigDecimal getValue()
     {
-         return this.value.get();
+         return this.value.getValue();
     }
 
     public void setValue(BigDecimal val)
     {
-        this.setText(String.valueOf(val));
         this.value.set(val);
+        this.setText(val.toString());
+    }
+
+    public SimpleObjectProperty<BigDecimal> valueProperty() {
+        return value;
     }
 }
--- src/main/java/com/utilities/IntegerTextField.java
+++ src/main/java/com/utilities/IntegerTextField.java
@@ -1,22 +1,45 @@
 package com.utilities;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
 import javafx.util.converter.IntegerStringConverter;
 
 public class IntegerTextField extends TextField{
 
+    private SimpleIntegerProperty property;
+
     public IntegerTextField()
     {
         super();
         this.setTextFormatter(new TextFormatter<>(new IntegerStringConverter()));
+        property = new SimpleIntegerProperty(0);
+        this.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.isEmpty()) {
+                try {
+                    this.setValue(Integer.parseInt(newValue));
+                } catch (NumberFormatException e) {
+                    Alert alert = new Alert(Alert.AlertType.WARNING);
+                    alert.setTitle("Conversion error");
+                    alert.setHeaderText("An error occurred while entering the value");
+                    alert.setContentText("It seems that the you have entered wrong type of the value");
+                    alert.showAndWait();
+                    this.setText(oldValue);
+                }
+            }
+            else
+                this.setValue(0);
+        });
     }
 
     public Integer getValue()
     {
-        if (this.getText().isEmpty())
-            return null;
+        return property.getValue();
+    }
 
-        return Integer.parseInt(this.getText());
+    public void setValue(int val)
+    {
+        this.property.set(val);
     }
 }
