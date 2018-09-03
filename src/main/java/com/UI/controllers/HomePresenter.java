fc4a2e574c4d5aff52652891dfd7ca5ab92fe88b
Jakub Kustra <kuba.kustra@gmail.com>
2018-09-04 01:45:19 +0200

    Added code for deleting a product from template table and save the template in DB, small fixes

--- src/main/java/com/UI/controllers/HomePresenter.java
+++ src/main/java/com/UI/controllers/HomePresenter.java
@@ -14,6 +14,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Region;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -21,8 +23,12 @@ import org.springframework.stereotype.Controller;
 
 import java.math.BigDecimal;
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 public class HomePresenter implements Initializable {
@@ -55,6 +61,9 @@ public class HomePresenter implements Initializable {
 
     @FXML
     private TableColumn<Templates, BigDecimal> grossCol;
+
+    @FXML
+    private TableColumn<Templates, Templates> deleteCol;
 
     @FXML
     private Label totalLbl;
@@ -90,6 +99,7 @@ public class HomePresenter implements Initializable {
         initializeTable();
         setListViewProperties();
         loadCustomers();
+        datePicker.setValue(LocalDate.now());
     }
 
     private void setListViewProperties() {
@@ -125,6 +135,24 @@ public class HomePresenter implements Initializable {
         netCol.setCellValueFactory(c -> new ReadOnlyObjectWrapper<>(c.getValue().getProduct().getUnitNetPrice()));
         vatRateCol.setCellValueFactory(c -> new ReadOnlyObjectWrapper<>(c.getValue().getProduct().getVatRate()));
         quantityCol.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+        deleteCol.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue())); // looks weird, fix it
+        deleteCol.setCellFactory(param -> new TableCell<Templates, Templates>() {
+            private Button removeButton = new Button("", new ImageView(new Image(getClass()
+                    .getResourceAsStream("/images/icons8-clear-symbol-24.png"))));
+
+            @Override
+            protected void updateItem(Templates temp, boolean empty) {
+                super.updateItem(temp, empty);
+
+                if (empty) {
+                    setGraphic(null);
+                    setText(null);
+                } else {
+                    setGraphic(removeButton);
+                    removeButton.setOnAction(event -> templateTable.getItems().remove(temp));
+                }
+            }
+        });
     }
 
     @FXML
@@ -189,7 +217,7 @@ public class HomePresenter implements Initializable {
         symbolCol.setCellValueFactory(new PropertyValueFactory<>("symbol"));
         unitCol.setCellValueFactory(new PropertyValueFactory<>("unit"));
         cpuCol.setCellValueFactory(new PropertyValueFactory<>("unitNetPrice"));
-        vatRateCol.setCellValueFactory(new PropertyValueFactory<>("taxRate"));
+        vatRateCol.setCellValueFactory(new PropertyValueFactory<>("vatRate"));
         tableView.getColumns().addAll(nameCol, symbolCol, unitCol, cpuCol, vatRateCol);
         tableView.getItems().setAll(productService.findAll());
         dialog.getDialogPane().setContent(tableView);
@@ -223,9 +251,28 @@ public class HomePresenter implements Initializable {
     }
 
     @FXML
-    void saveTemplate()
-    {
-        templatesService.saveAll(templateTable.getItems());
-        customersList.getSelectionModel().getSelectedItem().setTemplates(templateTable.getItems());
+    void saveTemplate() {
+        // if there is anything to compare with...
+        if (customersList.getSelectionModel().getSelectedItem() != null &&
+                !customersList.getSelectionModel().getSelectedItem().getTemplates().isEmpty()) {
+            // ...find out if there is any difference between templates in Customer object itself
+            // and what is currently in the table view
+            Set<Integer> ids = templateTable.getItems().stream()
+                    .map(Templates::getId)
+                    .collect(Collectors.toSet());
+            List<Templates> deletedTemplates = customersList.getSelectionModel().getSelectedItem()
+                    .getTemplates().stream()
+                    .filter(temp -> !ids.contains(temp.getId()))
+                    .collect(Collectors.toList());
+            if (!deletedTemplates.isEmpty()) {
+                templatesService.deleteInBatch(deletedTemplates);
+            }
+
+            if (!templateTable.getItems().isEmpty()) {
+                //update those that left, if any
+                templatesService.saveAll(templateTable.getItems());
+            }
+            customersList.getSelectionModel().getSelectedItem().setTemplates(templateTable.getItems());
+        }
     }
 }
